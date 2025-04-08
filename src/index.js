32823e8b876537d99b5b6c3bff9bc6572fbc8711
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-08 16:53:55 +0300

    fix db config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './config/db.config.js';
 
 dotenv.config();
 
@@ -10,6 +11,15 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
+
+app.get('/', async(req, res) => {
+    try{
+        const result = await pool.query('SELECT current_database()');
+        res.send(result.rows[0].current_database); 
+    }catch(err){
+        console.log(err);
+    }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
