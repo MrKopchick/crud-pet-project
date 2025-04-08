8f82eb775eb5a2f03ab1891db8d7c77ce575d594
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-08 17:48:51 +0300

    complete index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import pool from './config/db.config.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.config.js";
 
-import userRoutes from './routes/userRoutes.js';
-
-import errorHandling from './middlewares/errorHandler.js';  
+import userRoutes from "./routes/userRoutes.js";
+import errorHandling from "./middlewares/errorHandler.js";
+import createUserTable from "./ddl/createUserTable.js";
 
 dotenv.config();
 
@@ -15,19 +15,19 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
 
 app.use(errorHandling);
 
-app.get('/', async(req, res) => {
-    try{
-        const result = await pool.query('SELECT current_database()');
-        res.send(result.rows[0].current_database); 
-    }catch(err){
-        console.log(err);
-    } 
+createUserTable();
+
+app.get("/", async (req, res) => {
+  console.log("Start");
+  const result = await pool.query("SELECT current_database()");
+  console.log("result", result.rows);
+  res.send(`The database name is : ${result.rows[0].current_database}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http:localhost:${port}`);
 });
